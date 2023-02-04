9cf073e720eaa7f0b38f48494274bf1d01fc6918
alankm194 <alankm194@gmail.com>
2023-02-04 00:32:44 +0000

    refactor: refactor tests to use parameterized tests from CSV file

--- src/test/java/org/alan/RomanNumeralTest.java
+++ src/test/java/org/alan/RomanNumeralTest.java
@@ -1,48 +1,17 @@
 package org.alan;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RomanNumeralTest {
-    @Test
-    public void WhenIntegerIs1_thenReturnI() {
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/singleCharacterRomanNumeralTest.csv", numLinesToSkip = 1)
+    public void WhenIntegerHasSingleRomanNumeralCharacter_ThenReturnCharacter(int input, String expected) {
         RomanNumeralConverter romanNumConverter = new RomanNumeralConverter();
-        assertEquals("I", romanNumConverter.fromIntToNumeral(1));
+        assertEquals(expected, romanNumConverter.fromIntToNumeral(input));
     }
 
-    @Test
-    public void WhenIntegerIs5_thenReturnV() {
-        RomanNumeralConverter romanNumConverter = new RomanNumeralConverter();
-        assertEquals("V", romanNumConverter.fromIntToNumeral(5));
-    }
-
-    @Test
-    public void WhenIntegerIs10_thenReturnX() {
-        RomanNumeralConverter romanNumConverter = new RomanNumeralConverter();
-        assertEquals("X", romanNumConverter.fromIntToNumeral(10));
-    }
-
-    @Test
-    public void WhenIntegerIs50_thenReturnL() {
-        RomanNumeralConverter romanNumConverter = new RomanNumeralConverter();
-        assertEquals("L", romanNumConverter.fromIntToNumeral(50));
-    }
-
-    @Test
-    public void WhenIntegerIs100_thenReturnC() {
-        RomanNumeralConverter romanNumConverter = new RomanNumeralConverter();
-        assertEquals("C", romanNumConverter.fromIntToNumeral(100));
-    }
-
-    @Test
-    public void WhenIntegerIs500_thenReturnD() {
-        RomanNumeralConverter romanNumConverter = new RomanNumeralConverter();
-        assertEquals("D", romanNumConverter.fromIntToNumeral(500));
-    }
-
-    @Test
-    public void WhenIntegerIs1000_thenReturnD() {
-        RomanNumeralConverter romanNumConverter = new RomanNumeralConverter();
-        assertEquals("M", romanNumConverter.fromIntToNumeral(1000));
-    }
 }
